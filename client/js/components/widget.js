b0c0632e0d9878e44fb4091df1f7483d28f3fe1c
Quim Llimona <qllimona@lbl.gov>
2013-08-27 18:23:23 -0700

    Added widget method for binding without duplicate events

--- client/js/components/widget.js
+++ client/js/components/widget.js
@@ -3,69 +3,110 @@
 
 define(['entities', 'jquery-ui'], function(Entities) {
 
-	var Widget = function(id, context) {
-		var self = this;
+    var Widget = function(id, context) {
+        var self = this;
 
-		id = this.id = id || new Date().getTime();
-		var model = this.model = Entities.widget.create({
-			id: this.id,
-			'class': 'generic'
-		});
-		this.$el = $('<div id="'+this.id+'" class="widget"></div>');
-		this.$el.dialog({
-			stack: '.widget',
-			appendTo: context,
-			resize: function(event,ui) {
-				model.off('change:width', setWidth, self);
-				model.off('change:height', setHeight, self);
-				model.off('change:position', setPosition, self);
-				model.set({
-					width: ui.size.width,
-					height: ui.size.height,
-					position: JSON.stringify(ui.position)
-				});
-				model.on('change:width', setWidth, self);
-				model.on('change:height', setHeight, self);
-				model.on('change:position', setPosition, self);
-			},
-			drag: function(event,ui) {
-				model.off('change:position', setPosition, self);
-				model.set('position', JSON.stringify(ui.position));
-				model.on('change:position', setPosition, self);
-			},
-			focus: function(event,ui) {
-				model.off('change:lastFocus', setLayer, self);
-				model.set('lastFocus', new Date().getTime());
-				model.on('change:lastFocus', setLayer, self);
-			}
-		});
-		model.on('change:position', setPosition, this);
-		model.on('change:width', setWidth, this);
-		model.on('change:height', setHeight, this);
-		model.on('change:lastFocus', setLayer, this);
+        id = this.id = id || new Date().getTime();
+        var model = this.model = Entities.widget.create({
+            id: this.id,
+            'class': 'generic'
+        });
+        this.$el = $('<div id="'+this.id+'" class="widget"></div>');
+        this.$el.dialog({
+            stack: '.widget',
+            appendTo: context,
+            resize: function(event,ui) {
+                model.off('change:width', setWidth, self);
+                model.off('change:height', setHeight, self);
+                model.off('change:position', setPosition, self);
+                model.set({
+                    width: ui.size.width,
+                    height: ui.size.height,
+                    position: JSON.stringify(ui.position)
+                });
+                model.on('change:width', setWidth, self);
+                model.on('change:height', setHeight, self);
+                model.on('change:position', setPosition, self);
+            },
+            drag: function(event,ui) {
+                model.off('change:position', setPosition, self);
+                model.set('position', JSON.stringify(ui.position));
+                model.on('change:position', setPosition, self);
+            },
+            focus: function(event,ui) {
+                model.off('change:lastFocus', setLayer, self);
+                model.set('lastFocus', new Date().getTime());
+                model.on('change:lastFocus', setLayer, self);
+            }
+        });
 
-		return this;
-	};
+        model.on('change:position', setPosition, this);
+        model.on('change:width', setWidth, this);
+        model.on('change:height', setHeight, this);
+        model.on('change:lastFocus', setLayer, this);
 
-	// Private methods instantiated only once
+        return this;
+    };
 
-	var setPosition = function(model, position) {
-		position = JSON.parse(position);
-		this.$el.dialog('option','position', [position.left, position.top]);
-	};
+    Widget.prototype.bind = function(opts) {
 
-	var setWidth = function(model, width) {
-		this.$el.dialog('option','width', width);
-	};
+        /* EXAMPLE: call with defaults
+        this.bind({
+            model: this.model,
+            attribute: 'position',
+            source: this.$el,
+            event: 'resize',
+            setter: function(event, ui) {
+                return ui.value;
+            },
+            callback: function(model, value) {
+                this.$el.set(value);
+            }
+        }); */
 
-	var setHeight = function(model, height) {
-		this.$el.dialog('option','height', height);
-	};
+        var self = this,
+            model = opts.model || this.model,
+            attr = opts.attribute || opts.event,
+            source = opts.source || this.$el,
+            event = opts.event,
+            setter = opts.setter || function(model, value) {
+                source.html(value);
+            },
+            callback = opts.callback || function(event, ui) {
+                return ui.value;
+            },
+            set = function() {
+                model.off(remote, callback, self);
+                setter(arguments);
+                model.on(remote, callback, self);
+            };
 
-	var setLayer = function(model, index) {
-		this.$el.dialog('moveToTop');
-	};
+        this.bindings.push(opts);
+        var remote = 'change:' + attr;
+        model.on(remote, callback, self);
+        source.on(event, set);
 
-	return Widget;
+    };
+
+    // Private methods defined only once
+
+    var setPosition = function(model, position) {
+        position = JSON.parse(position);
+        this.$el.dialog('option','position', [position.left, position.top]);
+    };
+
+    var setWidth = function(model, width) {
+        this.$el.dialog('option','width', width);
+    };
+
+    var setHeight = function(model, height) {
+        this.$el.dialog('option','height', height);
+    };
+
+    var setLayer = function(model, index) {
+        this.$el.dialog('moveToTop');
+    };
+
+    return Widget;
 
 });
